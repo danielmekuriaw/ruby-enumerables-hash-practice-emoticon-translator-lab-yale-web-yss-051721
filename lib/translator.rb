1a1bcb1015d979972a82b2f4428a673d83c32b5f
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-06 02:26:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,7 @@ def get_japanese_emoticon(path, english_emoticon)
     if value[:english] == english_emoticon
         puts value[:japanese]
         return value[:japanese]
+    end
   }
   "Sorry, that emoticon was not found"
 end
